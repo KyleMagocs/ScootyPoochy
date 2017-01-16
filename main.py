fb96b5ece8d106994d28be4c73f5fd66a2b74dce
Kyle Magocs <kylemagocs@gmail.com>
2017-01-16 13:44:32 -0500

    Adding other (empty) context files, because

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ import pygame
 
 if __name__ == "__main__":
     pygame.init()
+
+    # THIS WOULD BE CALLED FROM A PLAYER SELECT CONTEXT, WHEREIN IT WOULD MAKE A SHITLOAD MORE SENSE
     game = GameContext((TestCharacter(), TestCharacter()))
     game.run_game()
