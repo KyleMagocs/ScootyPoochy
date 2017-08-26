ebf83b4b7c86ffaad396c7d3d909a4c64cd0cb03
Kyle Magocs <kylemagocs@gmail.com>
2017-08-26 00:37:03 -0400

    fixed some graphical bugs

--- objects/World.py
+++ objects/World.py
@@ -215,27 +215,27 @@ class World:
 
         # TODO:  This is dumb and gross
         if player.z < other_player.z:
-            for sprite in [x for x in self.level.walls if x.y + 32 < player.y]:
+            for sprite in [x for x in self.level.walls if x.y + 32 < player.visible_y]:
                 sprite.draw_part_two(screen, x_offset, y_offset)
             player.draw_as_player(screen, x_offset, player_y_offset)
-            for sprite in [x for x in self.level.walls if x.y+32>=player.y]:
+            for sprite in [x for x in self.level.walls if x.y+32>=player.visible_y]:
                 sprite.draw_part_two(screen, x_offset, y_offset)
 
-            for sprite in [x for x in self.level.walls if x.y + 32 < other_player.y]:
+            for sprite in [x for x in self.level.walls if x.y + 32 < other_player.visible_y]:
                 sprite.draw_part_two(screen, x_offset, y_offset)
             other_player.draw_normal(screen, x_offset, y_offset)
-            for sprite in [x for x in self.level.walls if x.y+32>=other_player.y]:
+            for sprite in [x for x in self.level.walls if x.y+32>=other_player.visible_y]:
                 sprite.draw_part_two(screen, x_offset, y_offset)
         else:
-            for sprite in [x for x in self.level.walls if x.y + 32 < other_player.y]:
+            for sprite in [x for x in self.level.walls if x.y + 32 < other_player.visible_y]:
                 sprite.draw_part_two(screen, x_offset, y_offset)
             other_player.draw_normal(screen, x_offset, y_offset)
-            for sprite in [x for x in self.level.walls if x.y+32>=other_player.y]:
+            for sprite in [x for x in self.level.walls if x.y+32>=other_player.visible_y]:
                 sprite.draw_part_two(screen, x_offset, y_offset)
-            for sprite in [x for x in self.level.walls if x.y + 32 < player.y]:
+            for sprite in [x for x in self.level.walls if x.y + 32 < player.visible_y]:
                 sprite.draw_part_two(screen, x_offset, y_offset)
             player.draw_as_player(screen, x_offset, player_y_offset)
-            for sprite in [x for x in self.level.walls if x.y+32>=player.y]:
+            for sprite in [x for x in self.level.walls if x.y+32>=player.visible_y]:
                 sprite.draw_part_two(screen, x_offset, y_offset)
 
         for sprite in self.level.broken_objects:
@@ -268,7 +268,7 @@ class World:
 
     def draw_win_text(self, screen, x_offset, color, text= 'FINISH !'):
         font = pygame.font.SysFont('Impact', 70)
-        text = textOutline(font, 'FINISH !', color,
+        text = textOutline(font, text, color,
                            colors.black)
         text.get_width()
         screen.blit(text, (x_offset + self.width / 2 - text.get_width() / 2, vars.SCREEN_HEIGHT / 2 - 10))
