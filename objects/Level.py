43bcfdc105583be116d2caa6e4f005c5634fdf50
Kyle Magocs <kylemagocs@gmail.com>
2017-09-01 21:08:43 -0400

    Fixing some more graphical things

--- objects/Level.py
+++ objects/Level.py
@@ -114,7 +114,7 @@ class ButtLevel(Level):
         self.width = self.theme.background_sprite.get_width()
 
         my_rooms = Room_Finish, Room_Three, Room_One, Room_Two, Bathroom, Room_Three, Backyard, Room_Start
-        my_doors = 0,           75,         75,       150,      75,       150,        75,       100
+        my_doors = 0,           75,         75,       150,      75,       150,        350,      350
 
         # my_rooms = Room_Finish, Room_Start
         # my_doors = 0,           100
