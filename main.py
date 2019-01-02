5a36010c79ea52dc4ffe388fc17299e1795aba03
Kyle Magocs <kylemagocs@gmail.com>
2019-01-02 14:32:21 -0500

    dumping exception logs now

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from contexts.glob import GlobalContext
 from contexts.level_select import LevelSelectContext
 from contexts.startup import StartupContext
 from vars import SCREEN_WIDTH, SCREEN_HEIGHT, LAUNCH_OPTIONS
+import datetime
+import sys, traceback
 
 size = width, height = SCREEN_WIDTH, SCREEN_HEIGHT
 
@@ -23,10 +25,17 @@ if __name__ == "__main__":
     #
     # level_select = LevelSelectContext(screen)
     # level = level_select.main_loop()
+    try:
 
-    startup = StartupContext(screen)
-    startup_result = startup.display_startup()
-    if not startup_result:
-        quit()
-    global_context = GlobalContext(screen)
-    global_context.main_loop()
+        startup = StartupContext(screen)
+        startup_result = startup.display_startup()
+        if not startup_result:
+            quit()
+        global_context = GlobalContext(screen)
+        global_context.main_loop()
+    except Exception as e:
+        with open('error_logs/Crash - ' + str(datetime.datetime.now().strftime('%y-%b-%d_%H-%M-%S')) + '.txt', 'w+') as f:
+            f.write(str(e))
+            f.write('\n\n\nTRACEBACK:\n\n')
+            f.write(repr(traceback.extract_tb(sys.exc_info()[2])))
+
